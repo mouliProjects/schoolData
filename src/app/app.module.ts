3bf8698cd64dc4ca13e8e00a7920afebc751ccdd
mouliProjects <pasilamouli.999@gmail.com>
2018-02-11 00:10:53 +0530

    login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
+
+const routes: Routes = [
+    {path : '' , component: LoginComponent},
+    {path : 'home', component: HomeComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'sideMenu', component: SideMenuComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SideMenuComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
